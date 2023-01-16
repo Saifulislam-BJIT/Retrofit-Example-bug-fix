31d99fef940205f493e5bdc9e50d2eb1a1d43af3
Saiful Islam <saiful.islam@bjitacademy.com>
2023-01-16 14:58:17 +0600

    initial commit

---
+++ app/src/main/java/com/bjit/retrofitexample/viewmodel/OverviewFragment.kt
@@ -0,0 +1,94 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bjit.retrofitexample.viewmodel
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.bjit.retrofitexample.adapter.PhotoGridAdapter
+import com.bjit.retrofitexample.databinding.FragmentOverviewBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+
+class OverviewFragment : Fragment() {
+
+    private val viewModel: OverviewViewModel by viewModels()
+    private var _binding: FragmentOverviewBinding? = null
+    private val binding get() = _binding!!
+
+    companion object {
+        var haveInternet = false
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentOverviewBinding.inflate(inflater, container, false)
+
+        // Allows Data Binding to Observe LiveData with the lifecycle of this Fragment
+        binding.lifecycleOwner = this
+
+        // Giving the binding access to the OverviewViewModel
+        binding.viewModel = viewModel
+
+        // Sets the adapter of the photosGrid RecyclerView
+        binding.photosGrid.adapter = PhotoGridAdapter()
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        MaterialAlertDialogBuilder(requireContext())
+            .setTitle("Internet Check")
+            .setMessage("Please, First see if there is a internet")
+            .setPositiveButton("Ok") { _, _ ->
+                haveInternet = isOnline()
+                viewModel.getMarsPhotos()
+            }
+            .show()
+    }
+
+    private fun isOnline(): Boolean {
+        val context = requireContext()
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (connectivityManager != null) {
+            val capabilities =
+                connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+            if (capabilities != null) {
+                if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
+                    Log.i("Internet", "NetworkCapabilities.TRANSPORT_CELLULAR")
+                    return true
+                } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
+                    Log.i("Internet", "NetworkCapabilities.TRANSPORT_WIFI")
+                    return true
+                } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)) {
+                    Log.i("Internet", "NetworkCapabilities.TRANSPORT_ETHERNET")
+                    return true
+                }
+            }
+        }
+        return false}
+}
